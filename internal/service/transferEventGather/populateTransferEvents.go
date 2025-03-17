214e173a89cd03508ec110176a4e939d71c79086
pborgen <pborgennc@gmail.com>
2025-03-17 14:43:41 -0400

    lower block number

--- internal/service/transferEventGather/populateTransferEvents.go
+++ internal/service/transferEventGather/populateTransferEvents.go
@@ -42,7 +42,7 @@ func Start() {
 		panic(err)
 	}
 
-	maxAmountOfBlocksToProcess := uint64(50)
+	maxAmountOfBlocksToProcess := uint64(10)
 	largestBlockNumber, err := transferEventService.GetLargestBlockNumber()
 	if err != nil {
 		panic(err)
